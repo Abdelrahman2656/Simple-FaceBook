bfdba4fabc5d4a977801eb43b9e7c30c6b06ef3f
Abdelrahman Elmonged <a.elmonged870@gmail.com>
2024-07-28 22:39:58 +0300

    add

--- api/Database/dbconnection.js
+++ api/Database/dbconnection.js
@@ -1,4 +1,4 @@
-import { Sequelize, DataTypes} from "sequelize";
+import { Sequelize} from "sequelize";
 //Database:
 const sequelize = new Sequelize('mysql://ukrl2aq1djc5n45g:2XjNUz8zN4mdgzXWGbcz@bgpj0gbkedzis273xcws-mysql.services.clever-cloud.com:3306/bgpj0gbkedzis273xcws');
 
--- api/modules/Posts/posts.model.js
+++ api/modules/Posts/posts.model.js
@@ -1,6 +1,7 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../../Database/dbconnection.js";
 
+
 export const postModel = sequelize.define(
   "post",
   {
